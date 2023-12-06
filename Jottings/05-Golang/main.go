336494b225896c30ddae2962f319fc404100749a
Nishant Giri <nishantgiri19722000@gmail.com>
2023-12-06 14:42:56 +0530

    golang intro : file added

---
+++ Jottings/05-Golang/main.go
@@ -0,0 +1,10 @@
+// Go also Golang is a compiled language.
+
+package main						//pakage installation is done by :  pakage keyword before the "pakage name"
+
+import "fmt"						/* import is used to include code from other packages.
+									Here fmt stands for formating for input and output. */
+
+func main() {						// using "main" is to declare the code lines are executable.
+	fmt.Println("Hello World!")		//Hence there will be no need to call function explicitly for each line of exection.
+}
